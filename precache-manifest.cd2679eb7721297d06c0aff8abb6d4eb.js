c204274502246e93ed68bde850fcbc9e1b674fc9
man3in3action <ychnagasivareddy@gmail.com>
2018-12-08 18:01:39 +0530

    Updates

---
+++ precache-manifest.cd2679eb7721297d06c0aff8abb6d4eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81312d4a81e499095a3d",
+    "url": "/dashboard/static/js/runtime~main.81312d4a.js"
+  },
+  {
+    "revision": "5b327c0307dcd1dbaf2e",
+    "url": "/dashboard/static/js/main.5b327c03.chunk.js"
+  },
+  {
+    "revision": "dada4efe13e1b9df36e6",
+    "url": "/dashboard/static/js/1.dada4efe.chunk.js"
+  },
+  {
+    "revision": "5b327c0307dcd1dbaf2e",
+    "url": "/dashboard/static/css/main.974d64c5.chunk.css"
+  },
+  {
+    "revision": "7983a1ce30897dfc70dd99f2f4ce9c46",
+    "url": "/dashboard/index.html"
+  }
+];
